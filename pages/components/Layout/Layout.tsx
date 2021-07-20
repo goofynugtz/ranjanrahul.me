cf84e3497df748887a6abfc8848a295075b08e95
goofyNugtz <rahulranjan25.RR@gmail.com>
2021-07-20 13:02:50 +0530

    Added font preloading

--- pages/components/Layout/Layout.tsx
+++ pages/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import useDarkMode, { DarkMode } from 'use-dark-mode';
 
+import Head from 'next/head';
+
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import React from 'react';
@@ -7,11 +9,37 @@ import React from 'react';
 
 export default function Layout({ children }: any) {
 
-    const darkMode:DarkMode = useDarkMode(true);
+    const darkMode: DarkMode = useDarkMode(true);
 
     return (
         <>
-            <Navbar {...darkMode}/>
+            <Head>
+                <link
+                    rel='preload'
+                    href='fonts/SFPro/sfpro-text-regular.ttf'
+                    as='font'
+                    crossOrigin=''
+                />
+                <link
+                    rel='preload'
+                    href='fonts/SFPro/sfpro-display-regular.ttf'
+                    as='font'
+                    crossOrigin=''
+                />
+                <link
+                    rel='preload'
+                    href='fonts/Overpass/overpass-regular.ttf'
+                    as='font'
+                    crossOrigin=''
+                />
+                <link
+                    rel='preload'
+                    href='fonts/SFPro/sfmono-regular.ttf'
+                    as='font'
+                    crossOrigin=''
+                />
+            </Head>
+            <Navbar {...darkMode} />
             <main className='main'>{children}</main>
             <Footer />
         </>
