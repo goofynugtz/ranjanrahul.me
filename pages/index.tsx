0e203b70a6eed5098fa6d5af28f17614ce53f78d
goofynugtz <rahulranjan25.RR@gmail.com>
2022-07-18 22:12:26 +0530

    Added Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Rahul Ranjan</title>
       </Head>
       <Layout>
         <Header />
